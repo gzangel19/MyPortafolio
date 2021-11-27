297505a78710271e4e37082c4c385cddb8a69227
Angel Guillermo Gauna <agauna32@gmail.com>
2021-11-27 20:06:35 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+       itemSelector: '.portfolio-item'
+    });
+ 
+    $('#portfolio-filters li').on('click', function() {
+       $("#portfolio-filters li").removeClass('filter-active');
+       $(this).addClass('filter-active');
+ 
+       portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+       });
+    });
+
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      gallery: {
+         enabled: true,
+         navigateByImgClick: true
+      }
+   });
+
+   $("#navbarNav").on('show.bs.collapse', function() {
+
+      $(".navbar").addClass("navbar-fixed");
+
+      $('a.nav-link, a.btn-custom').click(function() {
+         $("#navbarNav").collapse('hide');
+      });
+   });
+ 
+ 
+ });
